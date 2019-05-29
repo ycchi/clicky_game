f9debf3a226a4202d2c3d0e893230d902ab4477c
Youngchan Chi <YCCHI@YCs-MBP.local>
2019-05-29 15:28:07 -0400

    initial commit

---
+++ build/precache-manifest.5a36e998f4da0762327e5ebdef29166f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e0907e2786b7fe7ccb52d130c675e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85e6e01032f566df77bc",
+    "url": "/static/css/main.171fcdd8.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "85e6e01032f566df77bc",
+    "url": "/static/js/main.5a83d8ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
